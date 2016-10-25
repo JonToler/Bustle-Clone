c7a34e0a4d54ffda50fdcdf1b1db7cdc2382fcdf
Angie Smith and Jon Toler <angie@>
2016-10-25 15:16:59 -0700

    add description and image properties to firebase, add style, add navbar.

--- app/models/blog.js
+++ app/models/blog.js
@@ -2,6 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
+  description: DS.attr(),
+  image: DS.attr(),
   link: DS.attr(),
   title: DS.attr(),
   type: DS.attr()
